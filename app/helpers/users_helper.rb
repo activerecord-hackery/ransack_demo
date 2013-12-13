03a98503e9fb96964df7a36795d01eefc0f2ba29
jonatack <jonnyatack@gmail.com>
2013-12-13 13:10:35 +0100

    Refactor/DRY results partial

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,49 @@
 module UsersHelper
 
-  def present_results(count)
-  	if count > 10
-  		"Your first 10 results out of #{count} total"
+  def model_fields
+    # which fields to display and sort by
+    [:id, :first_name, :last_name, :email]
+  end
+
+  def results_limit
+    # max number of search results to display
+    10
+  end
+
+  def display_query_sql(users)
+    "SQL: #{users.to_sql}"
+  end
+
+  def display_results_header(count)
+  	if count > results_limit
+  		"Your first #{results_limit} results out of #{count} total"
   	else
   		"Your #{pluralize(count, 'result')}"
   	end
+  end
+
+  def action
+    action_name == 'advanced_search' ? :post : :get
+  end
+
+  def display_sort_column_headers(search)
+    model_fields.each_with_object('') do |field, string|
+  		string << content_tag(:th, sort_link(search, field, {}, method: action))
+  	end
+
+  end
+
+  def display_search_results(objects)
+    objects.limit(results_limit).each_with_object('') do |object, string|
+	    string << content_tag(:tr, display_search_results_row(object))
+    end
+  end
+
+  def display_search_results_row(object)
+    model_fields.each_with_object('') do |field, string|
+      string << content_tag(:td, object.send(field))
+    end
+    .html_safe
   end
 
 end
