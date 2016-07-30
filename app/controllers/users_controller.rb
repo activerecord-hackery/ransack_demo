1c74fee6a465c9a0a3e28d2e3fd621655f3f2b0f
Jon Atack <jonnyatack@gmail.com>
2016-07-30 16:37:04 +0200

    Remove unneeded responders methods in users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,13 @@
 class UsersController < ApplicationController
-  respond_to :html
-
   def index
     @search = User.search(params[:q])
     @users = ransack_result
-
-    respond_with @users
   end
 
   def advanced_search
     @search = User.search(params[:q])
     @search.build_grouping unless @search.groupings.any?
     @users = ransack_result
-
-    respond_with @users
   end
 
   private
