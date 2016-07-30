6726f56c257be3e9f3f91957cdeb5279433f2a98
Jon Atack <jonnyatack@gmail.com>
2016-07-30 16:50:13 +0200

    Extract #ransack call to method, change missed #search call

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,20 @@
 class UsersController < ApplicationController
   def index
-    @search = User.ransack(params[:q])
-    @users = ransack_result
+    @search = ransack_params
+    @users  = ransack_result
   end
 
   def advanced_search
-    @search = User.search(params[:q])
+    @search = ransack_params
     @search.build_grouping unless @search.groupings.any?
-    @users = ransack_result
+    @users  = ransack_result
   end
 
   private
+    def ransack_params
+      User.ransack(params[:q])
+    end
+
     def ransack_result
       if params[:distinct].to_i.zero?
         @search.result
