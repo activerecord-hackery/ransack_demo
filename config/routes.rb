28835c8b9a0255090efdd4e78d614a9de7a7fbba
Jon Atack <jonnyatack@gmail.com>
2016-08-12 15:29:34 +0200

    Update config files for Rails 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
-RansackDemo::Application.routes.draw do
+Rails.application.routes.draw do
   resources :users, only: :index do
     match 'advanced_search' => 'users#advanced_search',
           on: :collection, via: [:get, :post], as: :advanced_search
   end
   root to: 'users#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
