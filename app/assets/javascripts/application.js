dcb5745863d185ec7a81fbff8ce28769f7f8fcdc
Jon Atack <jonnyatack@gmail.com>
2016-08-12 18:16:04 +0200

    Require assets in application.js/.css and wrap at 80 chars.
    Why doesn't Rails respect its own 80-char line wrap rule?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,11 @@
-// This is a manifest file that'll be compiled into including all the files listed below.
-// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
-// be included in the compiled file accessible from http://example.com/assets/application.js
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// This is a manifest file that'll be compiled into including all the files
+// listed below. Add new JavaScript/Coffee code in separate files in this
+// directory and they'll automatically be included in the compiled file
+// accessible from http://example.com/assets/application.js. It's not advisable
+// to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
 //= require jquery2
 //= require jquery_ujs
-//= require_tree .
+//= require search
+// require_tree .
