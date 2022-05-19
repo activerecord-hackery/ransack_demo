69b2d0ec86f76aec276c4badca255eea2cdc106f
David Rodríguez <deivid.rodriguez@riseup.net>
2022-05-19 09:56:18 +0200

    Update some links to https

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
           on: :collection, via: [:get, :post], as: :advanced_search
   end
   root to: "users#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
